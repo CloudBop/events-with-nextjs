803a930c088d92380800eeabbb64abc6e3a469c4
Cloudbop <boppincolin@gmail.com>
2021-03-15 16:48:54 +0000

    added handler for programatically creating routes

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,24 @@
 // all events
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/events-search";
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
 
+  function fineEventsHandler(year, month) {
+    const uri = `/events/${year}/${month}/abc`;
+
+    // go here...
+    router.push(uri);
+  }
+
   return (
-    <div>
-      <h1>All Events Page</h1>
+    <>
+      <EventSearch onSearch={fineEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
 
