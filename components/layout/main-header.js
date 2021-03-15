a73316880956838935b475be8a9bca5a628efe9c
Cloudbop <boppincolin@gmail.com>
2021-03-15 17:11:05 +0000

    fixed uri in header

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -10,7 +10,7 @@ function MainHeader() {
       <nav className={classes.navigation}>
         <ul>
           <li>
-            <Link href="events">
+            <Link href="/events">
               <a>Browse All Events</a>
             </Link>
           </li>
