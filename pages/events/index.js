3a6fd1ca34eda177db9771a2035163a937b3f759
Cloudbop <boppincolin@gmail.com>
2021-03-15 16:32:55 +0000

    added AllEvents component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,13 @@
 // all events
-
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
     <div>
       <h1>All Events Page</h1>
+      <EventList items={events} />
     </div>
   );
 }
